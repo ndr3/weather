aedfe17a4dc346b95b636e4c523f6068b77f1323
Andreea Matyas <andreea.matyas@garmin.com>
2017-03-21 14:06:39 +0200

    Add simple autocomplete textview for locations
    Use hardcoded locations for now. Added API Key for Openweathermap API.

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String API_KEY = "599f795795dc6a51ffe33c0a3fca858c";
+
+    private static final String[] LOCATIONS = new String[] {
+            "Cluj-Napoca", "Jibou", "Toplita"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_location);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, LOCATIONS);
+        textView.setAdapter(adapter);
+    }
+
+    public String getWeatherData(String location) {
+        return "";
     }
 }
