cee7047eb5fa0bc3ca83ed60b00da7850273d922
Andreea Matyas <andreea.matyas@garmin.com>
2017-04-19 13:49:18 +0300

    Add List View to Forecast page
    Add toggle button to Settings fragment.

--- WeatherApp/app/src/main/java/com/example/weatherapp/ForecastWeatherFragment.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/ForecastWeatherFragment.java
@@ -1,30 +1,30 @@
 package com.example.weatherapp;
 
-import android.graphics.Typeface;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.format.DateUtils;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.weatherapp.model.WeatherDTO;
-import com.google.android.gms.location.places.Place;
-
-import java.sql.Date;
-import java.text.DecimalFormat;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
 
 public class ForecastWeatherFragment extends Fragment implements IWeatherFragment {
 
     private WeatherDTO mWeatherData;
     private ListView mListView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ForecastWeatherFragment() {
         // Required empty public constructor
@@ -45,8 +45,11 @@ public class ForecastWeatherFragment extends Fragment implements IWeatherFragmen
                     + " " + String.valueOf(mWeatherData.list[i+2].temp.day) + " ℃";
         }
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.forecast_listview, forecastArray);
-        mListView.setAdapter(adapter);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.forecast_listview, forecastArray);
+//        mListView.setAdapter(adapter);
+
+        mAdapter = new MyAdapter(forecastArray);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -58,8 +61,166 @@ public class ForecastWeatherFragment extends Fragment implements IWeatherFragmen
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_forecast_weather, container, false);
-        mListView = (ListView) view.findViewById(R.id.day_list);
+        //mListView = (ListView) view.findViewById(R.id.day_list);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         return view;
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.forecast_listview, parent, false);
+            // set the view's size, margins, paddings and layout parameters ...
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        };
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
+    /**
+     * A simple {@link Fragment} subclass.
+     * Activities that contain this fragment must implement the
+     * {@link OnFragmentInteractionListener} interface
+     * to handle interaction events.
+     * Use the {@link SettingsFragment#newInstance} factory method to
+     * create an instance of this fragment.
+     */
+    public static class SettingsFragment extends Fragment {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_PARAM1 = "param1";
+        private static final String ARG_PARAM2 = "param2";
+
+        // TODO: Rename and change types of parameters
+        private String mParam1;
+        private String mParam2;
+        private ToggleButton mToggleButton;
+
+        private OnFragmentInteractionListener mListener;
+
+        public SettingsFragment() {
+            // Required empty public constructor
+        }
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SettingsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        public static SettingsFragment newInstance(String param1, String param2) {
+            SettingsFragment fragment = new SettingsFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_PARAM1, param1);
+            args.putString(ARG_PARAM2, param2);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            mToggleButton = (ToggleButton) getActivity().findViewById(R.id.togglebutton);
+
+            if (getArguments() != null) {
+                mParam1 = getArguments().getString(ARG_PARAM1);
+                mParam2 = getArguments().getString(ARG_PARAM2);
+            }
+        }
+
+        public void toggleClicked(View v) {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_settings, container, false);
+        }
+
+        // TODO: Rename method, update argument and hook method into UI event
+        public void onButtonPressed(Uri uri) {
+            if (mListener != null) {
+                mListener.onFragmentInteraction(uri);
+            }
+        }
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            if (context instanceof OnFragmentInteractionListener) {
+                mListener = (OnFragmentInteractionListener) context;
+            } else {
+    //            throw new RuntimeException(context.toString()
+    //                    + " must implement OnFragmentInteractionListener");
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mListener = null;
+        }
+
+        /**
+         * This interface must be implemented by activities that contain this
+         * fragment to allow an interaction in this fragment to be communicated
+         * to the activity and potentially other fragments contained in that
+         * activity.
+         * <p>
+         * See the Android Training lesson <a href=
+         * "http://developer.android.com/training/basics/fragments/communicating.html"
+         * >Communicating with Other Fragments</a> for more information.
+         */
+        public interface OnFragmentInteractionListener {
+            // TODO: Update argument type and name
+            void onFragmentInteraction(Uri uri);
+        }
     }
 }
--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.weatherapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ToggleButton;
 
 import com.example.weatherapp.model.WeatherDTO;
 import com.google.android.gms.common.api.Status;
@@ -22,12 +29,26 @@ import java.io.IOException;
 import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
-    private static String WEATHER_URL ="http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=%s&APPID=%s&units=metric";
+    private static String WEATHER_URL ="http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=%s&APPID=%s&units=%s";
     private static String OPENWEATHERMAP_API_KEY = "599f795795dc6a51ffe33c0a3fca858c";
+
+    static final int FORECAST_DAYS = 12;
+    static final String DEFAULT_UNITS = "metric";
+    static final String UNITS_PREFERENCE_KEY = "com.example.weatherapp.units";
 
     static final int NUM_ITEMS = 3;
     static final int PAGE_LIMIT = 2;
 
+    // tags used to attach the fragments
+    private static final String TAG_HOME = "home";
+    private static final String TAG_PHOTOS = "photos";
+    private static final String TAG_MOVIES = "movies";
+    private static final String TAG_NOTIFICATIONS = "notifications";
+    private static final String TAG_SETTINGS = "settings";
+    public static String CURRENT_TAG = TAG_HOME;
+    public static int NAV_ITEM_INDEX = 0;
+
+    private NavigationView mNavigationView;
     private ViewPager mViewPager;
     private FragmentStatePagerAdapter mAdapter;
     private Place mPlace;
@@ -56,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("TODAY"));
         tabLayout.addTab(tabLayout.newTab().setText("TOMORROW"));
@@ -68,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setOffscreenPageLimit(PAGE_LIMIT);
         mViewPager.setAdapter(mAdapter);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        setUpNavigationView();
 
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -93,10 +118,76 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setUpNavigationView() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        NAV_ITEM_INDEX = 0;
+                        CURRENT_TAG = TAG_HOME;
+                        break;
+                    case R.id.nav_photos:
+                        NAV_ITEM_INDEX = 1;
+                        CURRENT_TAG = TAG_PHOTOS;
+                        break;
+                    case R.id.nav_movies:
+                        NAV_ITEM_INDEX = 2;
+                        CURRENT_TAG = TAG_MOVIES;
+                        break;
+                    case R.id.nav_notifications:
+                        NAV_ITEM_INDEX = 3;
+                        CURRENT_TAG = TAG_NOTIFICATIONS;
+                        break;
+                    case R.id.nav_settings:
+                        NAV_ITEM_INDEX = 4;
+                        CURRENT_TAG = TAG_SETTINGS;
+                        break;
+                    default:
+                        NAV_ITEM_INDEX = 0;
+                        break;
+                }
+
+
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+
+                loadHomeFragment();
+
+                return true;
+            }
+        });
+    }
+
+
+    private Fragment getHomeFragment() {
+        switch (NAV_ITEM_INDEX) {
+            case 4:
+                // settings fragment
+                ForecastWeatherFragment.SettingsFragment settingsFragment = new ForecastWeatherFragment.SettingsFragment();
+                return settingsFragment;
+            default:
+                return null;
+        }
+    }
+
+    private void loadHomeFragment() {
+        Fragment fragment = getHomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
     private class RetrieveWeatherDataTask extends AsyncTask<Place, Void, Void> {
         @Override
         protected Void doInBackground(Place... params) {
-            String forecastUrl = String.format(WEATHER_URL, params[0].getName(), "12", OPENWEATHERMAP_API_KEY);
+            SharedPreferences prefs = getApplicationContext().getSharedPreferences("com.example.weatherapp", Context.MODE_PRIVATE);
+            String units = prefs.getString(UNITS_PREFERENCE_KEY, DEFAULT_UNITS);
+
+            String forecastUrl = String.format(WEATHER_URL, params[0].getName(), String.valueOf(FORECAST_DAYS),
+                    OPENWEATHERMAP_API_KEY, units);
             Request forecastRequest = new Request.Builder()
                     .url(forecastUrl)
                     .build();
